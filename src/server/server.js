5d9c456e1a2658042edfbcfca09e5a30ba0a931d
Mauro Hernandez <mauroDeveloper@hotmail.com>
2020-07-29 01:17:08 -0430

    configuracion basica de express

---
+++ src/server/server.js
@@ -0,0 +1,14 @@
+import express from "express";
+
+const app = express();
+
+app.get("*", (req, res) => {
+  res.send({
+    hello: "express",
+  });
+});
+
+app.listen(3000, (err) => {
+  if (err) console.log(err);
+  else console.log(`http://localhost:3000`);
+});
